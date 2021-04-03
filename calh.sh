c8f483cc474b0a2f0ef05e61aeb3eb883b99c1e9
Shafin Murani <66113323+shafinmurani@users.noreply.github.com>
2021-04-03 12:27:54 +0530

    Update calh.sh

--- calh.sh
+++ calh.sh
@@ -4,7 +4,10 @@ printf "Get ready to hack the planet!!\n"
 wget www.blackarch.org/strap.sh
 chmod +x strap.sh
 sudo ./strap.sh
-sudo pacman -S wireshark-qt docker virtualbox nmap hydra nikto xclip sublist3r dirsearch gobuster dirb dirbuster tor figlet toilet torbrowser-launcher make radare2 gdb kdenlive metasploit metasploit-autopwn airgeddon hashcat crunch mdk4 reaver beef hostapd lighttpd bettercap ettercap bully pixiewps dhcp ccze asleap john hostapd-wpe nftables mdk3 hcxtools hcxdumptool wireshark-cli brave stegsolve bpytop steghide stegcracker python3 python perl go terminator xterm compton plank exploitdb
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syyu
+sudo pacman -S wireshark-qt docker virtualbox nmap hydra nikto xclip sublist3r dirsearch gobuster dirb dirbuster tor figlet toilet torbrowser-launcher make radare2 gdb kdenlive metasploit metasploit-autopwn airgeddon hashcat crunch mdk4 reaver beef hostapd lighttpd bettercap ettercap bully pixiewps dhcp ccze asleap john hostapd-wpe nftables mdk3 hcxtools hcxdumptool wireshark-cli brave stegsolve bpytop steghide stegcracker python3 python perl go terminator xterm compton plank exploitdb vim nano sublime-text
 sudo rm -rf strap.sh
 printf "\n"
 printf "\n"
